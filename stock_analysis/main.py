75f92f8a809a6acb13d283230eedce99c5f79d44
eric <nameericchen@gmail.com>
2017-10-06 11:30:00 +0800

    add model file

--- stock_analysis/main.py
+++ stock_analysis/main.py
@@ -1,5 +1,7 @@
-from ui_stock_analysis import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
+
+from ui_stock_analysis import Ui_MainWindow
+from stock_analysis_model import Stock_Analysis_Model
 
 if __name__ == '__main__':
     import sys
@@ -8,6 +10,9 @@ if __name__ == '__main__':
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)    
     MainWindow.show()
+
+    mdoel = Stock_Analysis_Model()
+
     sys.exit(app.exec_())
 
 
---
+++ stock_analysis/stock_analysis_model.py
@@ -0,0 +1,5 @@
+class Stock_Analysis_Model(object):
+    def __init__(self):
+        print('model init')
+
+
